3e881d170eb96879bb1d4efd3559c50839d78712
stkr89 <sumit.tokkar@gmail.com>
2022-05-21 23:58:54 -0500

    added pick up date to return initiated event type

--- types/event_types.go
+++ types/event_types.go
@@ -1,7 +1,11 @@
 package types
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"time"
+)
 
 type ReturnInitiated struct {
-	ReturnID uuid.UUID `json:"returnID"`
+	ReturnID   uuid.UUID `json:"returnID"`
+	PickupDate time.Time `json:"pickupDate"`
 }
